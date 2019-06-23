f5f59d1e2a4a0da0e744b956cb34e140f854218a
Henrik___ <Henrik.Wenck@student.hpi.de>
2019-06-23 22:48:42 +0200

    Restructure

---
+++ src/main/java/com/hpi/msd/ExampleProducer.java
@@ -0,0 +1,84 @@
+package com.hpi.msd;
+
+import com.opencsv.CSVReaderHeaderAware;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.streams.StreamsConfig;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+
+/**
+ * Created by nicolashoeck on 30.05.19.
+ */
+public class ExampleProducer {
+    public static void main(String[] args) {
+        /*
+        Properties props = new Properties();
+    //    props.put("bootstrap.servers", "localhost:9092");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+    //    props.put("acks", "all");
+    //    props.put("retries", 0);
+    //    props.put("batch.size", 16384);
+    //    props.put("linger.ms", 1);
+    //    props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "myapps.RecordSerializer");
+
+
+        HashMap<String,String> map = new HashMap<>();
+        map.put("Ampelfarbe","gruen");
+        map.put("Geschwindigkeit",">50km/h");
+        map.put("Fahrzeug","fahren");
+
+        Record record = new Record(map);
+
+        Producer<String, Record> producer = new KafkaProducer<>(props);
+        for (int i = 0; i < 100; i++){
+            producer.send(new ProducerRecord<String, Record>("input", "record_seq", record));
+
+            System.out.println(record.getMap().toString());}
+        producer.close();
+        */
+        Properties props = new Properties();
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "com.hpi.msd.RecordSerializer");
+        Producer<String, Record> producer = new KafkaProducer<>(props);
+
+        try {
+            // Lese Zeile für Zeile ein. Ergebnis ist ein Dictionary mit Key = Attribut und Value = Attributausprägung
+            CSVReaderHeaderAware reader = new CSVReaderHeaderAware(new FileReader("/Users/nicolashoeck/KafkaMaven/streams.examples/src/main/java/myapps/KreditD.csv"));
+            Map<String, String> values;
+            while((values = reader.readMap()) != null){
+
+                // Zu versendener Record wird erstellt als Dictionary
+                HashMap<String,Integer> map = new HashMap<>();
+                for (Map.Entry<String,String> value: values.entrySet()) {
+                    map.put(value.getKey()+"_"+value.getValue(),1);}
+
+                // send record to topic
+                producer.send(new ProducerRecord<String, Record>("input", "record_seq", new Record(map)));
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Could not read file.");
+        }
+        producer.close();
+
+
+    }
+
+
+}
---
+++ src/main/java/com/hpi/msd/RecordSerializer.java
@@ -0,0 +1,42 @@
+package com.hpi.msd;
+
+import org.apache.kafka.common.serialization.Serializer;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.util.Map;
+
+
+/**
+ * Created by nicolashoeck on 31.05.19.
+ */
+public class RecordSerializer implements Serializer<Record> {
+
+
+    @Override
+    public void configure(Map map, boolean b) {
+
+    }
+
+    @Override
+    public byte[] serialize(String s, Record o) {
+        ByteArrayOutputStream byteStream = new ByteArrayOutputStream(5000);
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(new BufferedOutputStream(byteStream));
+            oos.writeObject(o.getMap());
+            oos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        byte[] sendBuf = byteStream.toByteArray();
+        return sendBuf;
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
