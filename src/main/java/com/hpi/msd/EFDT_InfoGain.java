d39e5d28b522d0e9b4f7d3a8a5ec402241785ec7
Henrik___ <Henrik.Wenck@student.hpi.de>
2019-06-28 14:00:50 +0200

    work status reconstituted

--- src/main/java/com/hpi/msd/EFDT_InfoGain.java
+++ src/main/java/com/hpi/msd/EFDT_InfoGain.java
@@ -1,72 +1,95 @@
 package com.hpi.msd;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.ListMultimap;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 
 public class EFDT_InfoGain {
     /* Class for calculating the InformationGain within a node */
 
-    public static double IG(ArrayList<Double> Attributevalue1, ArrayList<Double> Attributevalue2, ArrayList<Double> Label) {
-        /* Method for calculating the InformationGain */
-        double p0 = Label.get(0) / (Label.get(0) + Label.get(1));
-        double p1 = Attributevalue1.get(0) / (Attributevalue1.get(0) + Attributevalue1.get(1));
-        double p2 = Attributevalue2.get(0) / (Attributevalue2.get(0) + Attributevalue2.get(1));
-        double a = (Attributevalue1.get(0) + Attributevalue1.get(1)) / (Label.get(0) + Label.get(1));
-        double log2 = Math.log(2);
 
-        double HT = -p0 * Math.log(p0) / log2 - (1 - p0) * Math.log(1 - p0) / log2;
-        double HTa = -a * (p1 * Math.log(p1) / log2 + (1 - p1) * Math.log(1 - p1) / log2)
-                - (1 - a) * (p2 * Math.log(p2) / log2 + (1 - p2) * Math.log(1 - p2) / log2);
-        double IG = HT - HTa;
-        return IG;
-    }
+    public static HashMap<String, Double> IG(HashMap<String, Double> map) {
+        /* Method for preprocessing and InformationGain handling
+         returns dict(Attribute, InformationGain) (Nullsplit included) */
 
-    public static HashMap<String, Double> IG_Calc(HashMap<String, Double> map) {
-        /* Method for preprocessing which returns InformationGain-Dict */
+        ListMultimap<String, Double> multimap = ArrayListMultimap.create();
+        for (String key : map.keySet()) {
+
+            String delStr1 = "_0";
+            String delStr2 = "_1";
+            String key2 = key.replace(delStr1, "");
+            key2 = key2.replace(delStr2, "");
+
+            multimap.put(key2, map.get(key));
+        }
+
         ArrayList<String> AttributeList = new ArrayList<String>();
-        ArrayList<Double> Attributevalue1 = new ArrayList<Double>();
-        ArrayList<Double> Attributevalue2 = new ArrayList<Double>();
-        ArrayList<Double> Label = new ArrayList<Double>();
-        HashMap<String, Double> IG_calcs = new HashMap<String, Double>();
-        String Splitter = new String();
-
         for (String key : map.keySet()) {
             if (!AttributeList.contains(key.split("_")[1]) && !key.split("_")[1].equals("Label")) {
                 AttributeList.add(key.split("_")[1]);
             }
         }
 
+        ArrayList<List<Double>> Atts = new ArrayList<List<Double>>();
+        ArrayList<List<Double>> Lab = new ArrayList<List<Double>>();
+        HashMap<String, Double> IGS = new HashMap<String, Double>();
         for (String Att : AttributeList) {
-            Label.clear();
-            Attributevalue1.clear();
-            Attributevalue2.clear();
-            int i = 0;
-            for (String key : map.keySet()) {
+            Atts.clear();
+            Lab.clear();
+            for (String key : multimap.keySet()) {
                 if (key.split("_")[1].equals(Att)) {
-                    if (i == 0) {
-                        Splitter = key.split("_")[2];
-                        Attributevalue1.add(map.get(key));
-                    } else {
-                        if (key.split("_")[2].equals(Splitter)) {
-                            Attributevalue1.add(map.get(key));
-                        } else {
-                            Attributevalue2.add(map.get(key));
-                        }
-                    }
-                    i++;
+                    Atts.add(multimap.get(key));
                 }
                 if (key.split("_")[1].equals("Label")) {
-                    Label.add(map.get(key));
+                    Lab.add(multimap.get(key));
                 }
             }
-            double IG_value = IG(Attributevalue1, Attributevalue2, Label);
-            IG_calcs.put(Att, IG_value);
+            IGS.put(Att,IG_calc(Atts,Lab));
         }
-        double Nullsplit=Collections.min(Label)/Collections.max(Label);
-        IG_calcs.put("Nullsplit", Nullsplit);
-        return IG_calcs;
+
+        double Nullsplit=Collections.min(Lab.get(0))/Collections.max(Lab.get(0));
+        IGS.put("Nullsplit", Nullsplit);
+
+        return IGS;
+    }
+
+
+    public static double IG_calc(ArrayList<List<Double>> Atts, ArrayList<List<Double>> Lab) {
+        /* Method for calculating the InformationGain */
+
+        double log2 = Math.log(2);
+        double Labsum=(Lab.get(0).get(1)+Lab.get(0).get(0));
+
+        double p0 = (Lab.get(0).get(1)/Labsum);
+        double HT = -p0 * Math.log(p0) / log2 - (1 - p0) * Math.log(1 - p0) / log2;
+
+        double HTa=0;
+        for (List<Double> values: Atts){
+            double p1= values.get(0)/sum(values);
+            if (p1==0 || p1==1){
+                HTa-=0;
+            }
+            else {
+                HTa += - sum(values) / Labsum * ((p1 * Math.log(p1) / log2 + (1 - p1) * Math.log(1 - p1) / log2));
+            }
+        }
+
+        double IG = HT-HTa;
+        return IG;
+    }
+
+    public static double sum(List<Double> list) {
+        double sum = 0;
+
+        for (double i : list)
+            sum = sum + i;
+
+        return sum;
     }
 
     public static double FindGXa(HashMap<String, Double> IG_collection) {
@@ -92,15 +115,9 @@ public class EFDT_InfoGain {
         return number;
     }
 
-    public static double HoeffdingTreshold(double safety, double Numberofevents) {
-        /* Method for finding epsilon */
-        int R=2;
-        double epsilon=Math.sqrt((-R*R*Math.log(1-safety))/(2*Numberofevents));
-        return epsilon;
-    }
-
     public static double HoeffdingSplit(double GXa, double Nullsplit, double epsilon) {
         /* Method for testing Hoeffding Split Criterion */
+
         if (GXa-Nullsplit>epsilon){
             System.out.println("Split wird durchgeführt");
         }
@@ -111,18 +128,32 @@ public class EFDT_InfoGain {
         return 0;
     }
 
+    public static double HoeffdingTreshold(double safety, double Numberofevents) {
+        /* Method for finding epsilon */
+
+        int R=2;
+        double epsilon=Math.sqrt((-R*R*Math.log(1-safety))/(2*Numberofevents));
+        return epsilon;
+    }
+
+
+
     public static void main(String[] args) {
         /* main with example dict */
 
         HashMap<String, Double> map = new HashMap<String, Double>();
         map.put("node_Outlook_Sunny_0", 2.0);
         map.put("node_Outlook_Sunny_1", 3.0);
+        map.put("node_Outlook_Normal_0", 0.0);
+        map.put("node_Outlook_Normal_1", 4.0);
         map.put("node_Outlook_Rainy_0", 3.0);
-        map.put("node_Outlook_Rainy_1", 6.0);
+        map.put("node_Outlook_Rainy_1", 2.0);
         map.put("node_Temp_Hot_0", 2.0);
         map.put("node_Temp_Hot_1", 2.0);
+        map.put("node_Temp_Mild_0", 4.0);
+        map.put("node_Temp_Mild_1", 2.0);
         map.put("node_Temp_Cold_0", 3.0);
-        map.put("node_Temp_Cold_1", 7.0);
+        map.put("node_Temp_Cold_1", 1.0);
         map.put("node_Humidity_High_0", 4.0);
         map.put("node_Humidity_High_1", 3.0);
         map.put("node_Humidity_Normal_0", 1.0);
@@ -135,16 +166,14 @@ public class EFDT_InfoGain {
         map.put("node_Label_1", 9.0);
 
 
-        HashMap<String, Double> IG_collection = IG_Calc(map);
-        System.out.println(IG_collection);
+        HashMap<String,Double> IGs= IG(map);
+        System.out.println(IGs);
 
-        double GXa= FindGXa(IG_collection);
+
+        double GXa= FindGXa(IGs);
         double Numberofevents=Numberofevents(map);
-        double epsilon =  HoeffdingTreshold(0.95, Numberofevents);
+        double epsilon = HoeffdingTreshold(0.95, Numberofevents);
 
-
-        HoeffdingSplit(GXa,IG_collection.get("Nullsplit"),epsilon);
+        HoeffdingSplit(GXa,IGs.get("Nullsplit"),epsilon);
     }
-
-
 }
